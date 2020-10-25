22239e79d11d74b57187e18cb64214b09aa3dd68
NivekUCN <72078122+NivekUCN@users.noreply.github.com>
2020-10-25 18:05:01 -0300

    Escensa
    Escenas ya funcioanando, para la aplicacion PIXCOLITS

---
+++ Pix_prueba0/Assets/Menu_principal.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class Menu_principal : MonoBehaviour
+{
+    public void PlayNivel ()
+    {
+        SceneManager.LoadScene("Nivel_1");
+    }
+
+     public void PlayCreativo ()
+    {
+        SceneManager.LoadScene("Nivel_creativo");
+    }
+
+     public void ayuda_normal ()
+    {
+        SceneManager.LoadScene("ayuda_normal");
+    }
+
+    public void ayuda_creativo ()
+    {
+        SceneManager.LoadScene("ayuda_creativo");
+    }
+
+    public void MenuPrincipal ()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}
